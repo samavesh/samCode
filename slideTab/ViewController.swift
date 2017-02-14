e8c69060b189bf10082318c93594fab100540055
Samavesh Tiwari <samavesh@myBook.local>
2017-02-14 12:25:14 +0530

    first commit

---
+++ slideTab/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  slideTab
+//
+//  Created by Samavesh Tiwari on 16/11/16.
+//  Copyright © 2016 Samavesh Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topHeader: UIView!
+    @IBOutlet weak var sliderView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
